864930fab1b31a50e19675cef5c4fb40c55b5fc1
Marius Haugan <hauganmarius@gmail.com>
2017-01-30 12:41:41 +0100

    Minor code changes
    Combined DataType attributes in the model classes from multiple to
    single lines where necessary.

--- src/WebStore/Models/Customer.cs
+++ src/WebStore/Models/Customer.cs
@@ -7,20 +7,15 @@ namespace WebStore.Models
     {
         public int CustomerID { get; set; }
 
-        [Required]
-        [StringLength(63)]
-        [Display(Name = "Last name")]
+        [Required, StringLength(63), Display(Name = "Last name")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string LastName { get; set; }
 
-        [Required]
-        [StringLength(63)]
-        [Display(Name = "First name")]
+        [Required, StringLength(63), Display(Name = "First name")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string FirstName { get; set; }
         
-        [StringLength(127)]
-        [DataType(DataType.EmailAddress)]
+        [StringLength(127), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         // DateTime er alltid [Required]
--- src/WebStore/Models/Product.cs
+++ src/WebStore/Models/Product.cs
@@ -6,25 +6,21 @@ namespace WebStore.Models
     {
         public int ProductID { get; set; }
 
-        [Required]
-        [StringLength(63, MinimumLength = 1)]
+        [Required, StringLength(63, MinimumLength = 1)]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Category { get; set; }
 
-        [Required]
-        [StringLength(63)]
+        [Required, StringLength(63)]
         public string Manifacturer { get; set; }
 
-        [Required]
-        [StringLength(127)]
+        [Required, StringLength(127)]
         public string Name { get; set; }
         
         [StringLength(255, MinimumLength = 1)]
         public string Description { get; set; }
 
         // Decimal (value-typer) er alltid [Required]
-        [Range(1,99999)]
-        [DataType(DataType.Currency)]
+        [Range(1,99999), DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
